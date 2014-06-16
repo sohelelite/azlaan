4594198becf3a4363962565e1b50e568831b013c
SohelElite <sohel.shaikh.s@gmail.com>
2014-06-16 15:36:42 +0530

    Login Page Style

--- public/system/routes/system.js
+++ public/system/routes/system.js
@@ -3,6 +3,28 @@
 //Setting up route
 angular.module('mean.system').config(['$stateProvider', '$urlRouterProvider',
         function($stateProvider, $urlRouterProvider) {
+
+            // Check if the user is connected
+            var checkLoggedin = function($q, $timeout, $http, $location) {
+                // Initialize a new promise
+                var deferred = $q.defer();
+
+                // Make an AJAX call to check if the user is logged in
+                $http.get('/loggedin').success(function(user) {
+                    // Authenticated
+                    if (user !== '0') $timeout(deferred.resolve);
+
+                    // Not Authenticated
+                    else {
+                        $timeout(deferred.reject);
+                        $location.url('/login');
+                    }
+                });
+
+                return deferred.promise;
+            };
+
+
             // For unmatched routes:
             $urlRouterProvider.otherwise('/');
 
@@ -10,7 +32,10 @@ angular.module('mean.system').config(['$stateProvider', '$urlRouterProvider',
             $stateProvider              
                 .state('home', {
                     url: '/',
-                    templateUrl: 'public/system/views/index.html'
+                    templateUrl: 'public/system/views/index.html',
+                    resolve:{
+                        loggedin : checkLoggedin
+                    }
                 })
                 .state('auth', {
                     templateUrl: 'public/auth/views/index.html'
